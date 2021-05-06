4caf716b0ad4c259bba25406f75a5200fe8b74c9
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-05-06 18:55:56 +0100

    initialtemplate for betting game algorithm

---
+++ AlgorithmPracticeMay2021/Count/BettingGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPracticeMay2021
+{
+    public class BettingGame
+    {
+
+
+        public int minimumMoves(int N, int K)
+        {
+            int count = 0;
+            int remainder = 0;
+            do
+            {
+                remainder = N / 2;
+                count++;
+                Console.WriteLine($"count { count } remainder {remainder}");
+
+            } while (count < K && remainder > 2);
+
+            return 1;
+        }
+    }
+}
