c90b6ae78b9a8b0e71d5833cda182b0802a019d5
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-05-07 04:57:05 +0100

    new algorithm exercises for practice. string counting and tree

---
+++ AlgorithmPracticeMay2021/Exercise/ExerciseM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPracticeMay2021
+{
+    public class ExerciseM
+    {
+
+        public int solution(string S, int[] C)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            int costs = 0;
+            // loop through the string length
+            for(int x =0; x < S.Length - 1; x++)
+            {
+                // get next character in string
+                int next = x + 1;
+                // check the string length is not exceeded
+                if(next< S.Length)
+                {
+                    Console.WriteLine($"current {S[x]} next {S[next]}");
+                    // compare characters
+                    if (S[x] == S[next])
+                    {
+                        // add lowest cost to total cost
+                        costs += C[x] < C[next] ? C[x] : C[next];
+                    }
+                }
+            }
+            Console.WriteLine($"Final Cost is {costs}");
+            return 1;
+        }
+    }
+    public class Tree2
+    {
+        public int x;
+        public Tree2 l;
+        public Tree2 r;
+        public Tree2(int _data)
+        {
+            x = _data;
+        }
+        public Tree2(int _data, Tree2 left, Tree2 right)
+        {
+            x = _data;
+            l = left;
+            r = right;
+        }
+    };
+}
--- AlgorithmPracticeMay2021/Program.cs
+++ AlgorithmPracticeMay2021/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AlgorithmPracticeMay2021.Tree;
 using System;
+using System.Collections.Generic;
 
 namespace AlgorithmPracticeMay2021
 {
@@ -9,7 +10,102 @@ namespace AlgorithmPracticeMay2021
         {
             Console.WriteLine("Tree Practice");
             //TestTrees();
-            TestBettingGame();
+            //TestBettingGame();
+            TestExercise2();
+
+        }
+
+        public static void TestExercise()
+        {
+            ExerciseM game = new ExerciseM();
+            string S = "aaaa"; //"aabbcc"; //"abccbd";
+            int[] costs = { 3, 4, 5, 6 };//{1,2,1,2,1,2 }; // { 0,1,2,3,4,5};
+            game.solution(S,costs);
+        }
+        public static void TestExercise2()
+        {
+            Tree2 root = new Tree2(1, new Tree2(2), new Tree2(3));
+            root.l.l = new Tree2(3);
+            root.l.l.l = new Tree2(2);
+            root.l.r = new Tree2(6);
+            // rigth nodes 
+            root.r.l = new Tree2(3);
+            root.r.r = new Tree2(1);
+            root.r.r.l = new Tree2(5);
+            root.r.r.r = new Tree2(6);
+            Console.WriteLine($"node : {root.x}");
+            // print lefts
+            Console.WriteLine($"Left Paths");
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            dictionary.Add(root.x, 1);
+            checkNodes(root.l,dictionary,true);
+            // print rights
+            Console.WriteLine($"Right Paths");
+            dictionary = new Dictionary<int, int>();
+            dictionary.Add(root.x, 1);
+            checkNodes(root.r,dictionary, false);
+        }
+
+        private static void PrintTree(Tree2 root, bool isLeft)
+        {
+            if (root == null)
+                return;
+            Console.WriteLine($"node : {root.x} {(isLeft ? "Left" :" Right" )}" );
+            //Print Left
+            //checkNodes(root.l,true);
+            //print right
+            // PrintTree(root.r, false);
+
+        }
+        private static void checkNodes(Tree2 node, Dictionary<int, int> dictionary, bool isLeft)
+        {   
+            while(node != null)
+            {
+                if (!dictionary.ContainsKey(node.x))
+                    dictionary.Add(node.x, 1);
+                else
+                    break;
+                //check left
+                //
+                if (isLeft)
+                {
+                    if (node.l != null && !dictionary.ContainsKey(node.l.x))
+                    {
+                        node = node.l;
+                    }
+                    else if (node.r != null && !dictionary.ContainsKey(node.r.x))
+                    {
+                        node = node.r;
+
+                    }
+                    else
+                    {
+                        node = null;
+                    }
+                }
+                else
+                {
+                    if (node.r != null && !dictionary.ContainsKey(node.r.x))
+                    {
+                        node = node.r;
+                    }
+                    else if (node.l != null && !dictionary.ContainsKey(node.l.x))
+                    {
+                        node = node.l;
+                    }
+                    else
+                    {
+                        node = null;
+                    }
+
+                }
+            }
+
+            foreach (var key in dictionary.Keys)
+                Console.Write($"{key}, ");
+            Console.WriteLine();
+
+            Console.WriteLine($"Distinct path { dictionary.Keys.Count }, ");
 
         }
 
